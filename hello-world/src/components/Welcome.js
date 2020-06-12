a5555352fad8b63974871d068c09608df4df5f46
jv640 <vermajatin640@gmail.com>
2020-06-12 18:04:51 +0530

    addin comment

--- hello-world/src/components/Welcome.js
+++ hello-world/src/components/Welcome.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react'
 
 class Welcome extends Component {
     render () {
-        return <h1>Welcome {this.props.name} a.k.a. {this.props.heroName}.</h1>       // here we are returning jsx not simple HTML
+        return <h1>Welcome {this.props.name} a.k.a. {this.props.heroName}.</h1>       
+        // here we are returning jsx not simple HTML
     }
 }
 
