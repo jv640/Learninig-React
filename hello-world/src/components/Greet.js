3ebfc1c64680a9752ad1a3e6829848ba0d0e828b
jv640 <vermajatin640@gmail.com>
2020-06-13 16:12:29 +0530

    implemented funtion component

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Jatin </h1>
+// }
+
+const Greet = () => <h1> hello Jatin.</h1>      // arror function
+
+//we can export it with file name also for that we have to put export before const and then we have to export it 
+// in App.js with the same name with wch we gonna import it as
+// import anyname from 'location'
+// <anyname />
+
+export default Greet
